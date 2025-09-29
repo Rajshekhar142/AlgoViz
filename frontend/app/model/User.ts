e5e73fe738c9755a6cd2d90d7cebe9624e75351f
Rajshekhar Mishra <rajsheakharmishra001@gmail.com>
2025-09-29 18:47:59 +0530

    user model created..

--- frontend/app/model/User.ts
+++ frontend/app/model/User.ts
@@ -3,23 +3,73 @@ import mongoose , {Schema , Document} from "mongoose";
 // schema as for mongoose.schema
 // document in order to provide type safety in nextjs
 
-export interface Message extends Document{
-    content : string;
-    createdAt: Date;
+// export interface Message extends Document{
+//     content : string;
+//     createdAt: Date;
+// }
+
+export interface User extends Document{
+    username: string,
+    email: string,
+    password: string,
+    role: string,
+    isAdmin: boolean,
+    isVerified: boolean
 }
 
-// it is a schema but a specifc type of schema so schema<its type> = new Schema
-const messageSchema: Schema<Message> = new Schema({
-    content: {
+
+const userSchema : Schema<User> = new Schema({
+    username: {
         type: String,
         required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
+    password:{ 
+        type: String,
+        minlength: 8,
+        required: true
+    },
+    role : {
+        type: String,
+        enum: ['user' ,'admin'],
+        default: 'user'
+
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+        required: true,
+
+    },
+    isVerified:{
+        type: Boolean,
+        default: false,
         required: true
     }
 })
+
+// it is a schema but a specifc type of schema so schema<its type> = new Schema
+// const messageSchema: Schema<Message> = new Schema({
+//     content: {
+//         type: String,
+//         required: true,
+//     },
+//     createdAt: {
+//         type: Date,
+//         default: Date.now,
+//         required: true
+//     }
+// })
 
 
 // task create a custom interface that has to be used in algoviz project by refering the above code ..
@@ -30,6 +80,6 @@ const messageSchema: Schema<Message> = new Schema({
 // follows messageSchema
 // mongoose.model<Message>  is used to provide type safety
 // mongoose.models.Message as mongoose.Model<Message>  is used to check if model already exists
-const messageModel = mongoose.models.Message as mongoose.Model<Message>  || mongoose.model<Message>('Message' , messageSchema);
-
-export default messageModel;
+// const messageModel = mongoose.models.Message as mongoose.Model<Message>  || mongoose.model<Message>('Message' , messageSchema);
+const userModel = mongoose.models.User as mongoose.Model<User> || mongoose.model<User>('User' , userSchema);
+export default userModel;
